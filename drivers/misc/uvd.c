32c5fb157ea4e41dc1b83fccd4546fb9eca2ab94
dnl.kim <dnl.kim@lge.com>
2020-02-13 20:49:46 +0900

    misc: uvd, notifier practice.a

--- drivers/misc/uvd.c
+++ drivers/misc/uvd.c
@@ -2,6 +2,9 @@
 #include <linux/uvd.h>
 #include <linux/platform_device.h>
 
+/* struct raw_notifier_head uvd_example_chain */
+static BLOCKING_NOTIFIER_HEAD(uvd_example_chain);
+
 static int uvd_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -14,6 +17,19 @@ static int __exit uvd_remove(struct platform_device *pdev)
 {
 	return 0;
 }
+
+static int uvd_example_event(struct notifier_block *nb,
+                             unsigned long event,
+                             void *ptr)
+{
+	printk(KERN_INFO "event %lu is fired!\n", event);
+
+	return NOTIFY_STOP;
+}
+
+static struct notifier_block uvd_example_notifier = {
+	.notifier_call = uvd_example_event,
+};
 
 static struct platform_driver uvd_driver = {
 	.probe  	= uvd_probe,
@@ -29,6 +45,9 @@ static struct platform_device uvd_device = {
 
 static int __init uvd_init(void)
 {
+	blocking_notifier_chain_register(&uvd_example_chain,
+                                         &uvd_example_notifier);
+
 	platform_driver_register(&uvd_driver);
 	platform_device_register(&uvd_device);
 	return 0;
